9c9a73a97033b88efd675448451134409cdcd9c0
Javier Santoyo <javier.santoyo@yahoo.com>
2023-06-13 01:33:15 -0600

    Create setUp of project #3

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -23,8 +23,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
+    window.localStorage.removeItem("board")
+    window.localStorage.removeItem("turn")
   }
 
   const updateBoard = (index) => {
